d0f42ed4bc2d6963cb0cd170ef3dd08b8f1ffe62
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2024-09-01 06:14:58 +0200

    doc: added doc to mongosettings

--- NoSQL.MongoDB.Api/Configurations/MongoDbSettings.cs
+++ NoSQL.MongoDB.Api/Configurations/MongoDbSettings.cs
@@ -2,11 +2,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NoSQL.MongoDB.Api.Configurations;
 
+/// <summary>
+/// MongoDB settings
+/// </summary>
 public class MongoDbSettings
 {
+    /// <summary>
+    /// MongoDB connection string
+    /// </summary>
     [Required]
     public string ConnectionString { get; init; }
-    
+
+    /// <summary>
+    /// MongoDB database name
+    /// </summary>
     [Required]
     public string DatabaseName { get; init; }
 }
