e770d6cc97fc4bc49f47865a43c9e328fc2a7e7c
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2024-09-01 06:15:42 +0200

    doc: updated doc

--- NoSQL.MongoDB.Api/Configurations/SectionName.cs
+++ NoSQL.MongoDB.Api/Configurations/SectionName.cs
@@ -1,6 +1,12 @@
 namespace NoSQL.MongoDB.Api.Configurations;
 
+/// <summary>
+/// Section name
+/// </summary>
 public static class SectionName
 {
+    /// <summary>
+    /// MongoDB section name in appsettings.json
+    /// </summary>
     public const string MongoDbSettings = "MongoDB";
 }
